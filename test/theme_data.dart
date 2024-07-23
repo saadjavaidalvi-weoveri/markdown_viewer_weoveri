8335ed83aacc2c3698ac3952b96582aa7caa50f5
Saad Alvi <85175211+saadjavaidalvi-weoveri@users.noreply.github.com>
2024-07-23 19:03:19 +0500

    Update theme_data.dart

--- test/theme_data.dart
+++ test/theme_data.dart
@@ -2,31 +2,31 @@ import 'package:flutter/material.dart';
 
 final themeData = ThemeData(
   textTheme: const TextTheme(
-    headline5: TextStyle(
+    headlineSmall: TextStyle(
       fontFamily: '.SF UI Display',
       fontWeight: FontWeight.w400,
       fontSize: 24.0,
       color: Color(0xdd000000),
     ),
-    headline6: TextStyle(
+    titleLarge: TextStyle(
       fontFamily: '.SF UI Display',
       fontWeight: FontWeight.w500,
       fontSize: 20.0,
       color: Color(0xdd000000),
     ),
-    subtitle1: TextStyle(
+    titleMedium: TextStyle(
       fontFamily: '.SF UI Text',
       fontWeight: FontWeight.w400,
       fontSize: 16.0,
       color: Color(0xdd000000),
     ),
-    bodyText1: TextStyle(
+    bodyLarge: TextStyle(
       fontFamily: '.SF UI Text',
       fontWeight: FontWeight.w500,
       fontSize: 14.0,
       color: Color(0xdd000000),
     ),
-    bodyText2: TextStyle(
+    bodyMedium: TextStyle(
       fontFamily: '.SF UI Text',
       fontWeight: FontWeight.w400,
       fontSize: 14.0,
